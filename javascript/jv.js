faa244d93ce91b55cb8fdbf3f23957137d9bb9c9
Hai27 <emailcuahao2004@gmail.com>
2024-09-24 16:48:53 +0700

    jv

---
+++ javascript/jv.js
@@ -0,0 +1,42 @@
+
+function myFunction (chieudai ) {
+    return chieudai * chieudai;
+}
+let result = myFunction (3);
+
+
+console.log(result)
+
+
+
+
+
+function boxSize (length, width ,height)  {
+    return length * width * height;
+}
+let bX = boxSize (5, 10, 15);
+
+console.log(bX)
+
+
+
+const pi = 3.14;
+
+
+function circleSquare ( R) {
+    return pi * R * 2;
+}
+let circleType = circleSquare (3.14, 5 );
+console.log(circleType)
+
+
+
+function pytagonSquare ( bottom, height ) 
+{
+    return bottom * 1/2 * height;
+}
+let pytagonType = pytagonSquare ( 3, 4 );
+console.log(pytagonType)
+
+
+
